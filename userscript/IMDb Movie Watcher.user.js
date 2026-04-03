3e1a935ef868fac527723256788b8cef1b17908d
Minecraft Schools <minecraft.schools95@gmail.com>
2026-04-03 11:28:18 -0500

    Update IMDb Movie Watcher.user.js

--- userscript/IMDb Movie Watcher.user.js
+++ userscript/IMDb Movie Watcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Movie Watcher
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Watch movies for free with IMDB
 // @author       You
 // @match        https://www.imdb.com/title/*
@@ -17,10 +17,14 @@
     let currentURL = window.location.href;
     let ImdbId = extractImdbId(currentURL)
     let redirectUrl = `https://vidfast.pro/movie/${ImdbId}?poster=true`
+    let redirectUrl1 = `https://vidfast.pro/tv/${ImdbId}/1/1?poster=true`
     setTimeout(() => {
         //setTimeout needed to allow a few ms for the new Btn to be injected onto page
         $('#mybutton').addEventListener('click', () => {
             window.open(redirectUrl);
+        });
+        $('#mybutton1').addEventListener('click', () => {
+            window.open(redirectUrl1);
         });
     },500);
 })();
@@ -37,11 +41,26 @@ function extractImdbId(url) {
 
 function init_css(){
     return `
+    <div id="richous">
     <button id="mybutton">Watch Now</button>
     <style>
         #mybutton{position:absolute;top:90px;left:45%;height:30px;width:120px;}
         #mybutton{background:#0073ea;color:white;padding-top:5px;text-align:center;}
         #mybutton{z-index:99999;}
+    </style>
+    <button id="mybutton1">(Click me if this is a Series)</button>
+    <style>
+        #mybutton1{position:absolute;top:140px;left:45%;height:30px;width:240px;}
+        #mybutton1{background:#0073ea;color:white;padding-top:5px;text-align:center;}
+        #mybutton1{z-index:99999;}
+    </style>
+    </div>
+    <style>
+        .richous{
+            display: flex;
+            justify-content: center; /* Centers horizontally */
+            align-items: center;
+        }
     </style>
     `;
 };
